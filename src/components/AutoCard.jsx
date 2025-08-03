1736b9313e07bb635b1c5449e54f867446d358c8
Maximiliano Orue <maximiliano95orue@gmail.com>
2025-08-03 15:15:34 -0300

    assets publico

--- src/components/AutoCard.jsx
+++ src/components/AutoCard.jsx
@@ -3,15 +3,13 @@ function AutoCard({ auto }) {
     <li className="card">
       <div className="image-container">
         <img
-          src={auto.image ? auto.image : "src/assets/image-not-found.png"}
+          src={auto.image ? auto.image : "assets/image-not-found.png"}
           alt=""
         />
       </div>
 
       <div className="content">
-        <p className="title">
-          {auto.marca} {auto.modelo}
-        </p>
+        <p className="title">{auto.marca} {auto.modelo}</p>
         <p>Tipo: {auto.tipo}</p>
         <p>Año: {auto.año}</p>
       </div>
